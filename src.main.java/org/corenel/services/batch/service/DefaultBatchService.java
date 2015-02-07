3711f73e1bce652e4421d079e67acb46406b1137
onjsdnjs <onjsdnjs@gmail.com>
2015-02-07 16:48:21 +0900

    2015.02.07 17

--- src.main.java/org/corenel/services/batch/service/DefaultBatchService.java
+++ src.main.java/org/corenel/services/batch/service/DefaultBatchService.java
@@ -2,6 +2,9 @@ package org.corenel.services.batch.service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * @author Á¤¼ö¿ø
+ */
 @SuppressWarnings({"serial"})
 @Service("batchService")
 public class DefaultBatchService implements BatchService {
