33d6566365ac9fba76f0569752e5c083d21a223e
onjsdnjs <onjsdnjs@gmail.com>
2015-02-12 14:17:17 +0900

    2015.02.12

--- src.main.java/org/corenel/core/camel/handler/ServiceDispatcherHandlerResolver.java
+++ src.main.java/org/corenel/core/camel/handler/ServiceDispatcherHandlerResolver.java
@@ -69,7 +69,7 @@ public class ServiceDispatcherHandlerResolver implements Processor{
 				disruptorExecutor.start();
 				disruptorHelper.handleService();
 				disruptorExecutor.setEventHandlerChain(null);
-				
+
 				Queue<ServiceHelper> serviceQueue = pipeline.getServiceQueue();
 				if(!serviceQueue.isEmpty()){
 					ProducerTemplate producer = exchange.getContext().createProducerTemplate();
